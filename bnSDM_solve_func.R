d2f1a07a7efc5d7191a330e78b27164515700c04
David <46323153+psijure@users.noreply.github.com>
2021-02-06 23:33:47 +0000

    Update bnSDM_solve_func.R

--- bnSDM_solve_func.R
+++ bnSDM_solve_func.R
@@ -74,6 +74,7 @@ bnSDM <- function(in_dir, out_dir = "BN_out/", focal, direction, method = "or")
   # Working cell by cell of raster
   for (i in 1:ncell(stack))
   {
+    print(i)
     if(is.na(focalValues[i]) == T) {outvals[i] <- NA
     # Run if focal species presence != 0
     } else if(focalValues[i] != 0)
