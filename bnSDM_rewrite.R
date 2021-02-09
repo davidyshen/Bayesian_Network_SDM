ddf21320c113cb2e7ba65ca10a3a81c1cf2e780c
David Shen <46323153+davidyshen@users.noreply.github.com>
2021-02-09 23:37:02 +0000

    Add files via upload

--- bnSDM_rewrite.R
+++ bnSDM_rewrite.R
@@ -1,3 +1,33 @@
+# David Shen
+# 09/02/2021
+# bnSDM_solve_func.R v2
+# A function that applies post-hoc bayesian networks to a species distribution
+# model
+# Based on Staniczenko et al. 2017    doi:10.111/ele.12770
+# ----------------------------------------------------------------------------------- #
+# Help ----
+# Function inputs:
+#   in_dir    = character string of directory of folder ending in "/" containing .tif input 
+#               raster SDMs of all species 
+#               e.g. "folder/"
+#   out_dir   = character string ending in "/" for output directory 
+#               e.g. "BN_out/"
+#   focal     = character string file name of focal species inside in_dir folder including
+#               file extension 
+#               e.g. "Gonimbrasia_belina.tif"
+#   direction = a vector of 1s and -1s that denote the direction of interaction of interacting
+#               species on focal species in alphabetical order 
+#               e.g. c(1, -1, 1)
+#   method    = character string "or" (default) or "and"
+#               specifies state table filling method OR vs AND (box 2 of Staniczenko et al)
+# 
+# SDM rasters will be loaded and used alphabetically
+
+# Required packages ----
+require(raster)
+require(dplyr)
+
+# Function ----
 bnSDM2 <- function(in_dir, out_dir = "BN_out/", focal, direction, method = "or")
 {
   if(!dir.exists(out_dir)){
@@ -31,8 +61,8 @@ bnSDM2 <- function(in_dir, out_dir = "BN_out/", focal, direction, method = "or")
   cat("\n Done \n")
   # Write posterior values to output raster
   raster::values(out) <- outvals
-  cat("Writing output to: ", paste0("~", out_dir, focalname, "_bnSDM.tif"), "... \n")
-  suppressWarnings(raster::writeRaster(out, paste0(out_dir, focalname, "_bnSDM.tif"), format = "GTiff", overwrite = T))
+  cat("Writing output to: ", paste0("~", out_dir, names(out), "_bnSDM.tif"), "... \n")
+  suppressWarnings(raster::writeRaster(out, paste0(out_dir, names(out), "_bnSDM.tif"), format = "GTiff", overwrite = T))
 }
 
 # Function that fills state table for interacting species ----
